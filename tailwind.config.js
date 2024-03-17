69ab4329c829013ff0edb7a3b829be575ae78851
Cristian Esteban Perrone <cristianperrone.dev@gmail.com>
2024-03-17 12:43:09 -0300

    Support/remove unuse variables (#7)
    * remove unuse variables
    
    * Improve content and component splitting
    
    * remove KeyboardManager reference

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  // darkMode: 'class',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents, theme }) {
+      addComponents({
+        'h1, h2, h3, h4': {
+          fontFamily: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+          fontWeight: theme('fontWeight.semibold'),
+        },
+      });
+    },
+  ],
 };
